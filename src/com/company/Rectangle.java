d0b6da9945b7ee85125184dbaecf656d0dfee4bf
VKozlova90 <fancygirl90@mail.ru>
2020-10-28 18:26:20 +0300

    v3

---
+++ src/com/company/Rectangle.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Rectangle {
+
+    double x;
+    double y;
+
+    public Rectangle (double x, double y){
+        System.out.println("Прямоугольник со сторонами:"+"x" + " "+"y");
+        }
+    if (x==y){
+        System.out.println("Квадрат со стороной:"+"x");
+    }
+    double calculateArea ();
+    double result = x*y;
+    return result;
+
+    }
+
